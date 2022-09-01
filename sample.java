fc0fc817a286350e28bd2e9225768117eae29009
Mahammad Afnan <muhammedafnan8184@gmail.com>
2022-09-01 12:58:11 +0530

    To check whether the entered String is palyndrome or not

---
+++ sample.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * To check whether the entered String is palyndrome or not
+ */
+public class sample {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string : ");
+
+        String s = "Ollo";
+        String newString = "";
+        char letter;
+
+        System.out.println("Original string is : " + s);
+
+        for (int i = 0; i < s.length(); i++) {
+
+            letter = s.charAt(i);
+            newString = letter + newString;
+
+        }
+        System.out.println("Reversed string is : " + newString);
+        
+        if(s.equalsIgnoreCase(newString)){
+            System.out.println("Palyndrome!");
+        }
+        else{
+            System.out.println("Not a palyndrome");
+        }
+
+
+        sc.close();
+    }
+
+}
