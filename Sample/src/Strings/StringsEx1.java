f3577e55a9007878f8f29561f242c22ae0ed5df5
Mahammad Afnan <muhammedafnan8184@gmail.com>
2022-12-09 20:51:55 +0530

    Strings stated.

---
+++ Sample/src/Strings/StringsEx1.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class StringsEx1 {
+
+    public static void main(String[] args) {
+
+        String name = "Afnan";
+        name = "s";
+        System.out.println("hello, " + name);
+    }
+
+}
