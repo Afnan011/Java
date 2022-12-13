4e71c17cbeaadc80311065d97e29e98fc3bf5543
Mahammad Afnan <muhammedafnan8184@gmail.com>
2022-12-13 20:42:31 +0530

    recursions started.

---
+++ Sample/src/RecursionEx.java
@@ -0,0 +1,30 @@
+public class RecursionEx {
+
+public static void fact(int n){
+    int k = 1;
+    for (int i = 2; i <=n ; i++) {
+        k = k * i;
+    }
+    System.out.println(n+"! = " + k);
+}
+
+public static void f(int n){
+    if (n <= 1){
+        return;
+    }
+    f(n-1);
+    System.out.println(n);
+    f(n-1);
+}
+
+    public static void main(String[] args) {
+
+        int n = 5;
+//        fact(n);
+
+        f(5);
+
+
+    }
+
+}
